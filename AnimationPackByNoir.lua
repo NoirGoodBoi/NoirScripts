68b629185da36b6ef3eac96abfb7d586343ae588
NoirGoodBoi <phapxover@gmail.com>
2025-07-28 20:47:20 +0700

    Create AnimationPackByNoir.lua

---
+++ AnimationPackByNoir.lua
@@ -0,0 +1,35 @@
+--[[ BY SUSSY
+Heres ALL SUPPORTED Animatations
+• Classic Animation packs
+1. Ninja
+2. Robot
+3.    —
+4. Levitate
+5. Mage
+6. Stylish
+7. Hero
+8. Toy
+9. Astronaut
+10. Bubbly
+11. Cartoony
+12. Elder
+13. Ghost
+14. Knight
+15. Vampire
+16. Werewolf
+17. Zombie
+18. Oldschool
+• New Animation packs
+1. Bold
+2. Adidas
+3. Catwalk
+4. Walmart
+5. Wicked
+6. NFL
+7. Pirate
+8. Adidas2 —(Adidas Community)— 
+]]
+getgenv().pack = "Adidas2" --Make sure you capitalize things right
+-- /e stop to stop the script btw
+ 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mautiku/Mautiku/refs/heads/main/AnimationCHANGER.txt", true))()
