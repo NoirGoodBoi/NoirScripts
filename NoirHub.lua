2b333842a523874d2311b96bf2ecb9db1f1248c1
NoirGoodBoi <phapxover@gmail.com>
2026-03-20 19:59:29 +0700

    Update loading subtitle in NoirHub.lua

--- NoirHub.lua
+++ NoirHub.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 local Window = Rayfield:CreateWindow({
     Name = "Noir Hub",
     LoadingTitle = "Loading NoirHub...",
-    LoadingSubtitle = "Script By Noir_Adono",
+    LoadingSubtitle = "Script By Noir",
     ConfigurationSaving = {
         Enabled = false,
     }
